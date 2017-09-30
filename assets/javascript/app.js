2a750eb185649119d3b2ccfcd1fb33a37e53b9de
Pawkit <patrickawong@gmail.com>
2017-09-29 20:28:49 -0700

    tidied up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// initialize firebase
   var config = {
     apiKey: "AIzaSyAG5_N0J-ba3DPXqj2cO7RPkOEDOLdhd1k",
     authDomain: "unique-asdf.firebaseapp.com",
@@ -10,40 +11,36 @@
 
 var database = firebase.database();
 
-// onclick function to add train information when submit is clicked
-
+// click function to add train information when submit is clicked
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
-  var trainName = $("#traininput").val().trim();                                          // user input
+// user input
+  var trainName = $("#traininput").val().trim();
   var trainD = $("#destinput").val().trim();
   var trainF = moment($("#firstTrainTime").val().trim()).format('HH:mm');
   var trainMin = $("#freqinput").val().trim();
 
-  
-  var newTrain = {                                                                      // creates variable called newTrain which is an object to hold data
+// creates variable newTrain to hold data
+  var newTrain = {
     name: trainName,
     dest: trainD,
     first: trainF,
     min: trainMin
   };
- 
 
+// uploads into database
+  database.ref().push(newTrain);                
 
-
-
-  database.ref().push(newTrain);                                                    // uploads into database
-
- 
+// debugger station
   console.log(newTrain.name);
   console.log(newTrain.dest);
-  console.log(newTrain.first);                                                  // holds console information for verification
+  console.log(newTrain.first);                                                  
   console.log(newTrain.min);
 
-  
-
+// clears text boxes after submit
   $("#traininput").val("");
-  $("#destinput").val("");                                                        // clears text boxes from userinput after they submit information
+  $("#destinput").val("");                                                        
   $("#firstTrainTime").val("");
   $("#freqinput").val("");
 });
@@ -52,21 +49,21 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
 
-                                                                                            // Store everything into a firefox variable
+// store everything into variables
   var trainName = childSnapshot.val().name;
   var trainD = childSnapshot.val().dest;
   var trainF = childSnapshot.val().first;
   var trainMin = childSnapshot.val().min;
 
-
+// train info
   console.log(trainName);
-  console.log(trainD);                                                                      // train info
+  console.log(trainD);                                                                      
   console.log(trainF);
   console.log(trainMin);
 
   var firstTime = "03:30";
 
-                                                                                          // calculations to give train arrival information
+// calculations for train arrival information
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
     console.log("First TIme" + firstTimeConverted);
 
@@ -88,7 +85,7 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
 
-                                                                                                              // Add each train's data into the table
+// Add each train's data into the table
+
   $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainD + "</td><td>" + trainMin + "</td><td>" + tMinutesTillTrain + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td></tr>");
 });
-
